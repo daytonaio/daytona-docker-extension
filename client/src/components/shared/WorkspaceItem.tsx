aa93c097f4487ad1d104c08452b1739e41b838bf
Dario Kriskovic <dario.kriskovic@gmail.com>
2024-09-25 13:37:17 +0200

    working on openEditor after onClick
    Signed-off-by: Dario Kriskovic <dario.kriskovic@gmail.com>

--- client/src/components/shared/WorkspaceItem.tsx
+++ client/src/components/shared/WorkspaceItem.tsx
@@ -1,4 +1,4 @@
-import { FC, useCallback, useEffect, useState } from 'react'
+import { FC, useCallback, useMemo, useState } from 'react'
 import {
   Box,
   Button,
@@ -11,6 +11,7 @@ import {
   DialogContent,
   DialogContentText,
   DialogActions,
+  CircularProgress,
 } from '@mui/material'
 import DeleteIcon from '@mui/icons-material/Delete'
 import { Workspace } from '../../api-client'
@@ -21,14 +22,23 @@ const WorkspaceItem: FC<{
   onDelete: (workspace: Workspace) => void
 }> = ({ workspace, onDelete }) => {
   const client = useDockerClient()
-  const [workspacePath, setWorkspacePath] = useState<string | null>(null)
+  const [openDialog, setOpenDialog] = useState(false)
+  const [loadingPath, setLoadingPath] = useState(false)
 
-  const isWorkspaceRunning = (workspace: Workspace) => {
-    return workspace.projects[0].state && workspace.projects[0].state.uptime > 0
+  const handleClickOpen = () => {
+    setOpenDialog(true)
   }
 
-  useEffect(() => {
-    getCodePath(workspace)
+  const handleClose = () => {
+    setOpenDialog(false)
+  }
+
+  const handleDelete = () => {
+    onDelete(workspace)
+  }
+
+  const isWorkspaceRunning = useMemo(() => {
+    return workspace.projects[0].state && workspace.projects[0].state.uptime > 0
   }, [workspace])
 
   const getCodePath = useCallback(
@@ -55,12 +65,52 @@ const WorkspaceItem: FC<{
             },
           )
         })
+
+        if (!path) {
+          console.log('2222')
+          await new Promise<void>((resolve, reject) => {
+            client?.extension.host?.cli.exec(
+              'daytona',
+              ['ssh', data.id, data.name, 'echo $HOME'],
+              {
+                stream: {
+                  onOutput: (message: any) => {
+                    if (message.stdout) {
+                      path = `${message.stdout.trim()}/${
+                        workspace.projects[0].name
+                      }`
+                    }
+                  },
+                  onClose: () => resolve(),
+                  onError: (error: any) => reject(error),
+                },
+              },
+            )
+          })
+        }
+
+        return path
       } catch (error) {
-        console.log(error, '-------')
+        console.log(error)
       }
     },
     [client],
   )
+
+  const openInVsCode = async () => {
+    setLoadingPath(true)
+    const path = await getCodePath(workspace)
+
+    setLoadingPath(false)
+    if (path) {
+      const a = document.createElement('a')
+      a.href = `vscode://vscode-remote/ssh-remote+default-${workspace.id}-${workspace.projects[0].name}${path}?windowId=_blank`
+      a.click()
+      a.remove()
+    } else {
+      client?.desktopUI.toast.error('Failed to open in editor')
+    }
+  }
 
   return (
     <ListItem divider>
@@ -79,8 +129,48 @@ const WorkspaceItem: FC<{
           <Typography
             color={isWorkspaceRunning(workspace) ? 'success.main' : 'error'}
           >
-            {isWorkspaceRunning(workspace) ? 'Running' : 'Stopped'}
-          </Typography>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <ListItem divider>
+        <Box
+          display={'flex'}
+          alignItems="center"
+          gap={2}
+          justifyContent="space-between"
+          width={'100%'}
+        >
+          <Box display={'flex'} alignItems="center" gap={3}>
+            <ListItemText>
+              <Typography variant="subtitle2">{workspace.name}</Typography>
+            </ListItemText>
+            <ListItemText secondary={workspace.projects[0].repository.url} />
+            <Typography color={isWorkspaceRunning ? 'success.main' : 'error'}>
+              {isWorkspaceRunning ? 'Running' : 'Stopped'}
+            </Typography>
+          </Box>
+          <Box display={'flex'} gap={2}>
+            {isWorkspaceRunning && (
+              <Button
+                startIcon={
+                  loadingPath && <CircularProgress size="16px" color="info" />
+                }
+                size="small"
+                variant="contained"
+                onClick={openInVsCode}
+              >
+                Open in VS code
+              </Button>
+            )}
+            <IconButton
+              aria-label="delete"
+              color="error"
+              onClick={handleClickOpen}
+            >
+              <DeleteIcon />
+            </IconButton>
+          </Box>
         </Box>
         {workspacePath && (
           <a
