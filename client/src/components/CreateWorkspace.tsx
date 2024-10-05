258cab6424b80a869b1c92165f169d86719746d9
Dario Kriskovic <dario.kriskovic@gmail.com>
2024-10-05 15:55:02 +0200

    started working on adding autocomplete input for repos

--- client/src/components/CreateWorkspace.tsx
+++ client/src/components/CreateWorkspace.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import {
+  Autocomplete,
   Box,
   Stepper,
   Step,
@@ -26,6 +27,26 @@ import { useDaytonaConfig } from '../providers/DaytonaConfigProvider'
 import WorkspaceList from './shared/WorkspaceList'
 
 const steps = ['Setup', 'Preparing', 'Ready']
+
+const top100Films = [
+  { title: 'The Shawshank Redemption', year: 1994 },
+  { title: 'The Godfather', year: 1972 },
+  { title: 'The Godfather: Part II', year: 1974 },
+  { title: 'The Dark Knight', year: 2008 },
+  { title: '12 Angry Men', year: 1957 },
+  { title: "Schindler's List", year: 1993 },
+  { title: 'Pulp Fiction', year: 1994 },
+  {
+    title: 'The Lord of the Rings: The Return of the King',
+    year: 2003,
+  },
+  { title: 'The Good, the Bad and the Ugly', year: 1966 },
+  { title: 'Fight Club', year: 1999 },
+  {
+    title: 'The Lord of the Rings: The Fellowship of the Ring',
+    year: 2001,
+  },
+]
 
 const CreateWorkspace = () => {
   const [activeStep, setActiveStep] = useState(0)
@@ -204,12 +225,18 @@ const CreateWorkspace = () => {
                           validate: validateURL,
                         }}
                         render={({ field, fieldState: { error } }) => (
-                          <TextField
-                            error={!!error}
-                            helperText={error?.message}
-                            placeholder="https://..., git@..."
-                            fullWidth
-                            {...field}
+                          <Autocomplete
+                            freeSolo
+                            options={top100Films.map((option) => option.title)}
+                            renderInput={(params) => (
+                              <TextField
+                                error={!!error}
+                                helperText={error?.message}
+                                placeholder="https://..., git@..."
+                                {...params}
+                                {...field}
+                              />
+                            )}
                           />
                         )}
                       />
